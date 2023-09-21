b65f17e3853263f8520539b58203948939fe0184
brendanillies <brendan.illies@gmail.com>
2023-09-20 21:22:35 -0700

    Add jQuery comments

--- starwars-character-viewer/scripts.js
+++ starwars-character-viewer/scripts.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
 
     $('button').on('click', function () {
+        // Get random character number to fetch
         const randomNumber = Math.floor(Math.random() * 88) + 1;
 
+        // Get character data
         $.get(`https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/${randomNumber}.json`, function(data) {
 
+            // Set character name
             $('h1').text(data['name']);
 
+            // Select image node
             const $img = $('img');
 
+            // Attempt to get character image from url and cache attempt
+            // If successful, add url to src attribute; else remove src attribute
             $.get({
                 url: data['image'],
                 cache: true
@@ -18,7 +24,6 @@ $(document).ready(function () {
                 })
                 .fail(function() {
                     $img.removeAttr('src');
-                    $img.attr('alt', 'Failed to load image');
                 })
 
         });
